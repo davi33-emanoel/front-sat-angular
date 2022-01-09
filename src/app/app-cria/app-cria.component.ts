22a76b80fa684bfcf9971f5b5838c3e1da5724c7
davi33-emanoel <emanoeldavi348@gmail.com>
2022-01-09 15:54:40 -0300

    first commit

---
+++ src/app/app-cria/app-cria.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Component({
+  selector: 'app-cria',
+  templateUrl: './app-cria.component.html',
+  styleUrls: ['./app-cria.component.scss'],
+})
+export class AppCriaComponent {
+  show = true;
+  nome: string = '';
+  email: string = '';
+  telefone: string = '';
+  cpf: string = '';
+  cnpj: string = '';
+  url = 'https://sat-project.herokuapp.com';
+  constructor(private httpClient: HttpClient) {}
+  postApi(
+    nome: string,
+    email: string,
+    cpf: string,
+    telefone: string,
+    cnpj: string
+  ) {
+    let obj = {
+      nome,
+      email,
+      cpf,
+      telefone,
+      cnpj,
+    };
+    this.httpClient
+      .post<any>(this.url + '/clients/save', obj, {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+          'Access-Control-Allow-Origin': 'http://localhost:4200',
+        }),
+      })
+      .subscribe((data) => {
+        console.log(data);
+        location.reload();
+      });
+  }
+}
